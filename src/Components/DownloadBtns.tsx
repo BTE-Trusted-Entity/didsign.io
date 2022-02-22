192535c53497a80a46117434f723fa0424c51141
usmangeek <usman@galaniprojects.de>
2022-02-22 16:23:45 +0100

    Added start over button

--- src/Components/DownloadBtns.tsx
+++ src/Components/DownloadBtns.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { selectFinalHash, selectHash } from '../Features/Signer/hashSlice'
-import { useAppSelector } from '../app/hooks'
-import { selectDid, selectSign } from '../Features/Signer/SignatureSlice'
+import {
+  clearHash,
+  selectFinalHash,
+  selectHash,
+} from '../Features/Signer/hashSlice'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import {
+  clearSign,
+  selectDid,
+  selectSign,
+} from '../Features/Signer/SignatureSlice'
 import { generateJWS, generateZipFile } from '../Utils/sign-helpers'
 import { Signature, SignDoc } from '../Utils/types'
 import { saveAs } from 'file-saver'
-import { selectFile } from '../Features/Signer/FileSlice'
+import {
+  clearAll,
+  clearFileName,
+  selectFile,
+} from '../Features/Signer/FileSlice'
 import '../Styles/App.css'
+import BtnStartOver from '../ImageAssets/button_start_over_NEW.svg'
 
 export function DownloadBtns() {
   const sign = useAppSelector(selectSign)
@@ -15,44 +28,51 @@ export function DownloadBtns() {
   const hashes = useAppSelector(selectHash)
   const files = useAppSelector(selectFile)
   const signature: Signature = { keyID: did, signature: sign }
-  let jws = ''
+
+  const dispatch = useAppDispatch()
 
   const handleDownloadSign = async () => {
-    jws = generateJWS(signature, await finalHash)
+    const jws = generateJWS(signature, await finalHash)
     const signedDoc: SignDoc = { hashes: hashes, jws: jws }
     const blob = new Blob([JSON.stringify(signedDoc)], {
       type: 'text/plain;charset=utf-8',
     })
-    saveAs(blob, 'DIDsign.signature')
+    saveAs(blob, 'signature.didsign')
   }
   const handleZip = async () => {
-    jws = generateJWS(signature, await finalHash)
     await generateZipFile(files)
   }
+  const handleStartOver = () => {
+    dispatch(clearSign())
+    dispatch(clearAll())
+    dispatch(clearHash())
+    dispatch(clearFileName())
+  }
   return (
-    <div>
-      <div className="bg-[#ddf0ff80] border-solid border-[#517ca240] border-2 mx-auto flex flex-col items-center justify-center rounded-b-lg w-[48%] big-phone:w-[80%] pt-4 pr-4 shadow-md mb-8">
-        <div className="w-1/2 h-10 2xl:w-[40%] 2xl:h-12 flex space-x-2 justify-center items-center">
-          <span className="pr-2 text-[14px] font-[Overpass Regular]">now</span>
-          <button
-            className="w-[160px] h-[30px] text-[14px]  font-[Overpass Regular] bg-sky-700 text-zinc-50 rounded-md"
-            onClick={handleZip}
-          >
-            ZIP ALL FILES
-          </button>
-        </div>
-        <div className="w-1/2 2xl:w-[36%] h-10 2xl:h-12 flex space-x-3 justify-start items-center">
-          <span className="text-[14px] 2xl:w-[36%] font-[Overpass Regular] text-right">
-            or only download
-          </span>
-          <button
-            className="w-[130px] h-[22px]  font-[Overpass Regular] bg-sky-700 text-zinc-50 text-[12px] rounded-md mt-1.5"
-            onClick={handleDownloadSign}
-          >
-            SIGNATURE
-          </button>
-        </div>
+    <div className="bg-[#ddf0ff80] border-solid border-[#517ca240] border-[1px] mx-auto flex flex-col items-center justify-center rounded-b-lg w-[48%] big-phone:w-[80%] pt-4 pr-4 shadow-sm mb-8 relative">
+      <div className="w-1/2 h-10 2xl:w-[40%] 2xl:h-12 flex space-x-2 justify-center items-center">
+        <span className="pr-2 text-[14px] font-[Overpass Regular]">now</span>
+        <button
+          className="w-[160px] h-[30px] text-[14px]  font-[Overpass Regular] bg-sky-700 text-zinc-50 rounded-md"
+          onClick={handleZip}
+        >
+          ZIP ALL FILES
+        </button>
       </div>
+      <div className="w-1/2 2xl:w-[36%] h-10 2xl:h-12 flex space-x-3 justify-start items-center">
+        <span className="text-[14px] 2xl:w-[36%] font-[Overpass Regular] text-right">
+          or only download
+        </span>
+        <button
+          className="w-[130px] h-[22px]  font-[Overpass Regular] bg-sky-700 text-zinc-50 text-[12px] rounded-md mt-1.5"
+          onClick={handleDownloadSign}
+        >
+          SIGNATURE
+        </button>
+      </div>
+      <button className="absolute -right-1 -bottom-1" onClick={handleStartOver}>
+        <img className="h-12 w-12" src={BtnStartOver} />
+      </button>
     </div>
   )
 }
--- src/Components/Verifyer/BottomSectionVerifyer.tsx
+++ src/Components/Verifyer/BottomSectionVerifyer.tsx
@@ -1,12 +1,16 @@
 import React, { useMemo, useRef } from 'react'
-import { useAppSelector } from '../../app/hooks'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import {
+  clearEndpoint,
   selectEndpointTypes,
   selectEndpointURL,
   selectVerifiedDid,
   selectVerifiedSign,
 } from '../../Features/Signer/EndpointSlice'
 import OkIcon from '../../ImageAssets/icon_oK.svg'
+import BtnStartOver from '../../ImageAssets/button_start_over_NEW.svg'
+import { clearAll, clearFileName } from '../../Features/Signer/FileSlice'
+import { clearHash } from '../../Features/Signer/hashSlice'
 
 export function BottomSectionVerifyer() {
   const sign = useAppSelector(selectVerifiedSign)
@@ -14,17 +18,25 @@ export function BottomSectionVerifyer() {
   const urls = useAppSelector(selectEndpointURL)
   const types = useAppSelector(selectEndpointTypes)
   const verificationRef = useRef<null | HTMLDivElement>(null)
+  const dispatch = useAppDispatch()
   const scrollToBottom = () => {
     if (verificationRef.current != null) {
       verificationRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }
   useMemo(scrollToBottom, [sign])
+  const handleStartOver = () => {
+    dispatch(clearEndpoint())
+    dispatch(clearAll())
+    dispatch(clearHash())
+    dispatch(clearFileName())
+  }
+
   function renderSignCom() {
     return (
       <div className="flex flex-col w-[90%] mx-auto ml-3 2xl:space-y-5 mt-10 mb-10 sm:space-y-5 big-phone:space-y-12">
         <div className="flex w-[90%] items-center justify-start space-x-5">
-          <p className=" text-green-600 font-[Overpass Regular] text-[16px]">
+          <p className=" text-green-600 font-['Overpass'] text-[16px]">
             Verification
           </p>
           <img src={OkIcon} />{' '}
@@ -33,30 +45,30 @@ export function BottomSectionVerifyer() {
           <p className="pr-4 w-[12%] text-[#2A2231] font-[Overpass Regular] lg:text-[16px] 2xl:text-[20px] big-phone:text-[12px]">
             Signature
           </p>
-          <span className=" tracking-tighter overflow-wrap break-words text-[#2A2231] font-[Overpass Regular] 2xl:text-[18px] lg:text-[14px] w-4/5 big-phone:text-[10px]">
+          <span className=" tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] 2xl:text-[18px] lg:text-[14px] w-4/5 big-phone:text-[10px]">
             {sign}
           </span>
         </div>
         <div className="w-full flex flex-wrap  lg:space-x-3 md:space-x-5 h-14 justify-start ">
-          <span className="pr-4 w-[12%] text-[#2A2231] font-[Overpass Regular] lg:text-[16px] big-phone:text-[12px] 2xl:text-[20px]">
+          <span className="pr-4 w-[12%] text-[#2A2231] font-['Overpass'] lg:text-[16px] big-phone:text-[12px] 2xl:text-[20px]">
             Signed By
           </span>
-          <span className="tracking-tighter overflow-wrap break-words text-[#2A2231] font-[Overpass Regular] lg:text-[14px] big-phone:text-[10px] w-4/5 2xl:text-[18px]">
+          <span className="tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] lg:text-[14px] big-phone:text-[10px] w-4/5 2xl:text-[18px]">
             {did}
           </span>
         </div>
         <div className="w-full flex flex-wrap lg:space-x-3 md:space-x-5 h-fit ">
-          <span className="pr-4 w-[12%]   tracking-tighter text-left text-[#2A2231] font-[Overpass Regular] lg:text-[16px] big-phone:text-[12px] 2xl:text-[20px]">
+          <span className="pr-4 w-[12%]   tracking-tighter text-left text-[#2A2231] font-['Overpass'] lg:text-[16px] big-phone:text-[12px] 2xl:text-[20px]">
             Service Endpoints
           </span>
 
           <div className="flex flex-col space-y-2 h-fit w-4/5">
             {urls.map((url: string, index: number) => (
               <div key={index} className="flex flex-col h-fit space-y-1 ">
-                <span className="tracking-tighter overflow-wrap break-words text-[#2A2231] font-[Overpass Regular] lg:text-[14px] big-phone:text-[10px]  w-full 2xl:text-[18px]">
+                <span className="tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] lg:text-[14px] big-phone:text-[10px]  w-full 2xl:text-[18px]">
                   {types[index]}
                 </span>
-                <span className=" tracking-tighter overflow-wrap break-words text-[#2A2231] font-[Overpass Regular] lg:text-[14px] big-phone:text-[10px] w-full 2xl:text-[18px]">
+                <span className=" tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] lg:text-[14px] big-phone:text-[10px] w-full 2xl:text-[18px]">
                   {url}
                 </span>
                 <div className=" border-b-[1px] border-b-gray-900 border-dotted w-full"></div>
@@ -71,7 +83,7 @@ export function BottomSectionVerifyer() {
     <div className=" bg-bottom-body w-screen relative">
       <div
         ref={verificationRef}
-        className={`bg-[#ddf0ff80] border-solid relative border-[#517ca240] border-2 rounded-b-lg min-h-[6rem] max-h-screen mx-auto w-[48%] big-phone:w-[80%] mb-8`}
+        className={`bg-[#ddf0ff80] border-solid relative border-[#517ca240] border-[1px] rounded-b-lg min-h-[6rem] max-h-screen mx-auto w-[48%] big-phone:w-[80%] mb-8 shadow-md`}
       >
         {sign != '' ? (
           renderSignCom()
@@ -81,6 +93,12 @@ export function BottomSectionVerifyer() {
             Verification
           </span>
         )}
+        <button
+          className="absolute -right-1 -bottom-1"
+          onClick={handleStartOver}
+        >
+          <img className="h-12 w-12" src={BtnStartOver} />
+        </button>
       </div>
     </div>
   )
