aa3b5583571855ae8ca98734bf2af41ca5592d4f
usmangeek <usman@galaniprojects.de>
2022-03-01 13:42:03 +0100

    Design changes

--- src/Components/Verifyer/BottomSectionVerifyer.tsx
+++ src/Components/Verifyer/BottomSectionVerifyer.tsx
@@ -38,17 +38,17 @@ export function BottomSectionVerifyer() {
   function renderVerificationError() {
     return (
       <div className="flex flex-col w-[90%] mx-auto ml-3 2xl:space-y-5 mt-10 mb-10 sm:space-y-5 big-phone:space-y-12">
-        <div className="flex w-full items-center justify-start lg:space-x-3 md:space-x-5">
+        <div className="flex w-full items-center justify-self-start md:space-x-3 big-phone:space-x-14">
           <p className=" text-[#F06543] font-['Overpass'] text-[16px] 2xl:text-[20px] w-[12%]">
             Verification
           </p>
           <img src={AttentionIcon} />{' '}
         </div>
-        <div className="w-full flex flex-wrap lg:space-x-3 md:space-x-5 h-14 justify-start">
-          <p className="pr-4 w-[12%] text-[#2A2231] font-['Overpass'] lg:text-[16px] 2xl:text-[20px] big-phone:text-[12px]">
+        <div className="max-w-full flex flex-wrap md:space-x-3 h-14 md:justify-start big-phone: justify-items-start big-phone:space-y-1">
+          <p className="pr-4 md:w-[12%] text-[#F06543] font-['Overpass'] md:text-[16px] 2xl:text-[20px] big-phone:text-[14px]">
             Attention
           </p>
-          <span className=" tracking-tighter overflow-wrap break-words text-[#F06543] font-['Overpass'] 2xl:text-[18px] lg:text-[14px] w-4/5 big-phone:text-[10px]">
+          <span className=" tracking-tighter overflow-wrap break-words text-[#F06543] font-['Overpass'] 2xl:text-[18px] md:text-[14px] w-4/5 big-phone:text-[12px]">
             The signature does not match with the imported files. Please make
             sure to import the correct files.
           </span>
@@ -61,41 +61,41 @@ export function BottomSectionVerifyer() {
       return renderVerificationError()
     } else {
       return (
-        <div className="flex flex-col w-[90%] mx-auto ml-3 2xl:space-y-5 mt-10 mb-10 sm:space-y-5 big-phone:space-y-12">
-          <div className="flex w-full items-center justify-start lg:space-x-3 md:space-x-5">
-            <p className=" text-green-600 font-['Overpass'] text-[16px] 2xl:text-[20px] w-[12%]">
+        <div className="flex flex-col w-[90%] mx-auto ml-3  mt-10 mb-10 space-y-[40px]">
+          <div className="flex w-full items-center justify-self-start space-x-[10px] big-phone:space-x-14">
+            <p className=" text-green-600 font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px] w-[12%]">
               Verification
             </p>
             <img src={OkIcon} />{' '}
           </div>
-          <div className="w-full flex flex-wrap lg:space-x-3 md:space-x-5 h-14 justify-start">
-            <p className="pr-4 w-[12%] text-[#2A2231] font-['Overpass'] lg:text-[16px] 2xl:text-[20px] big-phone:text-[12px]">
+          <div className="max-w-full flex w-[698px] space-x-[20px]">
+            <p className="text-[#2A2231] font-['Overpass'] text-[16px] leading-[22px] tracking-[0.11px]">
               Signature
             </p>
-            <span className=" tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] 2xl:text-[18px] lg:text-[14px] w-4/5 big-phone:text-[10px]">
+            <span className="overflow-wrap break-words w-4/5 h-[18px] text-[#2A2231] font-['Overpass'] font-bold text-[14px] leading-[22px] tracking-[0.1px]">
               {sign}
             </span>
           </div>
-          <div className="w-full flex flex-wrap  lg:space-x-3 md:space-x-5 h-14 justify-start ">
-            <span className="pr-4 w-[12%] text-[#2A2231] font-['Overpass'] lg:text-[16px] big-phone:text-[12px] 2xl:text-[20px]">
+          <div className="max-w-full  flex w-[698px] space-x-[20px] flex-wrap  ">
+            <p className="  text-[#2A2231] font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px]">
               Signed By
-            </span>
-            <span className="tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] lg:text-[14px] big-phone:text-[10px] w-4/5 2xl:text-[18px]">
+            </p>
+            <span className="overflow-wrap break-words h-[18px] w-4/5 text-[#2A2231] font-['Overpass'] font-bold text-[14px] leading-[22px] tracking-[0.1px]">
               {did}
             </span>
           </div>
-          <div className="w-full flex flex-wrap lg:space-x-3 md:space-x-5 h-fit ">
-            <span className="pr-4 w-[12%]   tracking-tighter text-left text-[#2A2231] font-['Overpass'] lg:text-[16px] big-phone:text-[12px] 2xl:text-[20px]">
+          <div className="max-w-full flex w-[698px] space-x-[20px]">
+            <span className="text-[#2A2231] w-[74px] text-right font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px]">
               Service Endpoints
             </span>
 
             <div className="flex flex-col space-y-2 h-fit w-4/5">
               {urls.map((url: string, index: number) => (
                 <div key={index} className="flex flex-col h-fit space-y-1 ">
-                  <span className="tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] lg:text-[14px] big-phone:text-[10px]  w-full 2xl:text-[18px]">
+                  <span className=" overflow-wrap break-words text-[#2A2231] font-['Overpass'] font-bold text-[14px] leading-[22px] tracking-[0.1px]">
                     {types[index]}
                   </span>
-                  <span className=" tracking-tighter overflow-wrap break-words text-[#2A2231] font-['Overpass'] lg:text-[14px] big-phone:text-[10px] w-full 2xl:text-[18px]">
+                  <span className="overflow-wrap break-words text-[#2A2231] font-['Overpass'] text-[14px] leading-[16px] tracking-[0.1px]">
                     {url}
                   </span>
                   <div className=" border-b-[1px] border-b-gray-900 border-dotted w-full"></div>
@@ -108,15 +108,15 @@ export function BottomSectionVerifyer() {
     }
   }
   return (
-    <div className=" bg-bottom-body w-screen relative">
+    <div className=" bg-bottom-body w-screen relative pl-2 pr-2">
       <div
         ref={verificationRef}
-        className={`bg-[#ddf0ff80] border-solid relative border-[#517ca240] border-[1px] rounded-b-lg min-h-[6rem] max-h-screen mx-auto w-[48%] big-phone:w-[80%] mb-8 shadow-md`}
+        className={`bg-[#ddf0ff80] border-solid relative border-[#517ca240] border-[1px] rounded-b-[15px] min-h-[8rem] max-h-screen mx-auto max-w-[766px] mb-4 shadow-md`}
       >
         {sign != '' ? (
           renderSignCom()
         ) : (
-          <span className="absolute top-2 left-6 text-[#2a223180] text-[16px] 2xl:text-[20px]">
+          <span className="absolute top-6 left-10 font-['Overpass'] text-[#2a223180] text-[16px] leading-[17px] tracking-[0.11px]">
             {' '}
             Verification
           </span>
