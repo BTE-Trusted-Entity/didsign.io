fbcb1fa939c0224113ff28786486f831aaeef720
usmangeek <usman@galaniprojects.de>
2022-03-01 13:41:39 +0100

    Design changes

--- src/Components/SignVerifyBtn.tsx
+++ src/Components/SignVerifyBtn.tsx
@@ -21,47 +21,55 @@ function SignVerifyBtn() {
   }
   return (
     <div className="bg-mid-body">
-      <div className="flex w-screen mx-auto h-10">
+      <div className="flex w-screen mx-auto h-[42px]">
         <div
           id="signer"
-          className={`flex flex-col items-center justify-center h-full w-1/2 ${
+          className={`pt-[3px] flex flex-col items-end justify-center h-full space-y-[3px] w-1/2 ${
             !userIsSigner && 'bg-selected-button'
           } `}
           onClick={handleSigner}
         >
           <button
-            className={` w-full bg-transparent font-normal text-[18px] uppercase big-phone:text-sm big-phone:h-1/2 3xl:text-2xl font-['Overpass'] ${
+            className={` w-[333px]  text-left bg-transparent font-normal text-[18px] leading-[20px] tracking-[0.16px] uppercase font-['Overpass'] ${
               !userIsSigner
-                ? 'text-[#3e6e9980] bg-selected-button'
+                ? 'text-[#3e6e9980] bg-selected-button text-[16px] leading-[17px]'
                 : 'text-[#2A2231]'
             } `}
           >
             SIGN
           </button>
-          <div
-            id="signer-line"
-            className={`bg-[#2A2231] w-1/4 ${
-              userIsSigner && 'h-[4px] rounded-md'
-            } `}
-          ></div>
+          <div className="w-[375px] flex justify-items-start">
+            <div
+              id="signer-line"
+              className={`w-1/3 bg-[#2A2231] ${
+                userIsSigner && 'h-[4px] rounded-md'
+              } `}
+            ></div>
+          </div>
         </div>
         <div
           id="verifyer"
-          className={`flex flex-col items-center justify-center h-full w-1/2 ${
+          className={`pt-[3px] flex flex-col items-start  justify-center h-full w-1/2 space-y-1 pb-[3px] ${
             userIsSigner && 'bg-selected-button'
           }`}
           onClick={handleSigner}
         >
           <button
-            className={` w-full bg-transparent font-normal text-[18px] uppercase big-phone:text-sm big-phone:h-1/2 3xl:text-2xl font-['Overpass']] ${
-              userIsSigner ? 'text-[#3e6e9980]' : 'text-[#2A2231]'
+            className={` w-[333px]  text-right  font-normal text-[17px] leading-[16px] tracking-[0.16px] uppercase font-['Overpass']] ${
+              userIsSigner
+                ? 'text-[#3e6e9980] text-[16px] leading-[17px]'
+                : 'text-[#2A2231]'
             }`}
           >
-            Verify
+            VERIFY
           </button>
-          <div
-            className={`bg-[#2A2231] w-1/4 ${!userIsSigner && 'h-[4px]'}`}
-          ></div>
+          <div className="w-[363px] flex justify-end">
+            <div
+              className={`w-1/3 bg-[#2A2231] ${
+                !userIsSigner && 'h-[4px] rounded-md'
+              } `}
+            ></div>
+          </div>
         </div>
       </div>
     </div>
