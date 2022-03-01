3e4564d3b3981da85c4d0bf0f5d601076a135420
usmangeek <usman@galaniprojects.de>
2022-03-01 13:41:54 +0100

    Design changes

--- src/Components/VerifyerComponent.tsx
+++ src/Components/VerifyerComponent.tsx
@@ -5,27 +5,39 @@ import { useAppSelector } from '../app/hooks'
 import { FileNameList } from './FileNameList'
 import { ZippedFile } from './ZippedFile'
 import { VerifierFileList } from './Verifyer/VerifierFileList'
+import CenterRightBubble from '../ImageAssets/CenterRightBubble.svg'
+import CenterLeftBubble from '../ImageAssets/CenterLeftBubble.svg'
+import { EmptyFilesList } from './EmptyFilesList'
 
 function VerifyerComponent() {
   const fileName = useAppSelector(selectFilename)
   const file = useAppSelector(selectFile)
 
   return (
-    <div className="bg-mid-body w-screen overflow-y-hidden overflow-x-hidden relative">
+    <div className="bg-mid-body w-screen overflow-y-hidden overflow-x-hidden relative bottom-0 pl-2 pr-2">
       <ImportFiles />
       <div
-        className={`scrollbar-thin scrollbar-thumb-sky-700 overflow-y-scroll relative flex-col -space-y-1 mx-auto w-[48%] min-h-[150px] 2xl:min-h-[200px] max-h-[900px]
-             big-phone:w-[80%] bg-[#ddf0ff80] border-solid border-[#517ca240] border-t-[1px] border-l-[1px] border-r-[1px]`}
+        className={`overflow-y-auto relative flex-col -space-y-1 mx-auto max-w-[766px] min-h-[211px] max-h-[900px]
+         bg-[#ddf0ff80] border-solid border-[#517ca240] border-t-[1px] border-l-[1px] border-r-[1px]`}
       >
         {file.length >= 0 && fileName.length === 0 && (
-          <span className="absolute top-2 left-8 text-[#2a223180] font-[Overpass Regular] text-[16px] 2xl:text-[20px]">
+          <span className="absolute top-4 left-10 text-[#2a223180] font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px] w-[34px]">
             Files
           </span>
         )}
         <ZippedFile />
-        {fileName.length > 0 ? <FileNameList /> : <VerifierFileList />}
-        <p></p>
+        {fileName.length > 0 && <FileNameList />}
+        {file.length > 0 && <VerifierFileList />}
+        {file.length === 0 && <EmptyFilesList />}
       </div>
+      <img
+        src={CenterLeftBubble}
+        className="absolute bottom-0 left-0 h-[105px] w-[343.5px]  pointer-events-none"
+      />
+      <img
+        src={CenterRightBubble}
+        className=" absolute bottom-0 right-0 h-[105px] w-[248px] pointer-events-none"
+      />
     </div>
   )
 }
