eb681253bd913c58f37528eef808b0b072048c78
usmangeek <usman@galaniprojects.de>
2022-03-01 13:39:38 +0100

    Removed bubbles

--- src/Components/DIDSign.tsx
+++ src/Components/DIDSign.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import '../Styles/App.css'
 import Header from './Header'
 import SignerComponent from './SignerComponent'
@@ -9,9 +9,8 @@ import { BottomSectionSigner } from './BottomSection'
 import { useAppSelector } from '../app/hooks'
 import { selectUser } from '../Features/Signer/UserSlice'
 import { BottomSectionVerifyer } from './Verifyer/BottomSectionVerifyer'
-import TopRightBubble from '../ImageAssets/TopRightBubble.svg'
-import TopLeftBubble from '../ImageAssets/TopLeftBubble.svg'
 import BottomLeftBubble from '../ImageAssets/BottomLeftBubble.svg'
+
 import BottomRightBubble from '../ImageAssets/BottomRightBubble.svg'
 
 function showSigner() {
@@ -24,15 +23,7 @@ export function DIDSign() {
   const userIsSigner = useAppSelector(selectUser)
 
   return (
-    <div className="relative min-h-screen w-screen overflow-y-scroll bg-bottom-body overflow-x-hidden flex flex-col scrollbar-thin scrollbar-thumb-sky-700">
-      <img
-        src={TopLeftBubble}
-        className="absolute top-0 left-0 h-[150px] big-phone:h-[60px] pointer-events-none"
-      />
-      <img
-        src={TopRightBubble}
-        className="absolute top-0 right-0 big-phone:h-[60px] pointer-events-none"
-      />
+    <div className="relative min-h-screen w-screen overflow-y-auto bg-bottom-body overflow-x-hidden flex flex-col">
       <Header />
       <SignVerifyBtn />
       {userIsSigner ? showSigner() : showVerifyer()}
@@ -40,11 +31,11 @@ export function DIDSign() {
       <Footer />
       <img
         src={BottomLeftBubble}
-        className="absolute bottom-0 left-0 w-1/4 big-phone:h-[120px] mt-auto pointer-events-none"
+        className="absolute bottom-0 left-0 h-[234px] w-[220px]  pointer-events-none"
       />
       <img
         src={BottomRightBubble}
-        className=" absolute bottom-0 right-0 w-1/4 big-phone:h-[120px] mt-auto pointer-events-none"
+        className=" absolute bottom-0 right-0 h-[234px] w-[220px] pointer-events-none"
       />
     </div>
   )
