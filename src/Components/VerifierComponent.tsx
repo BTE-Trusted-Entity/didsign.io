92561c013eb6b90c3a80cdf14b67a6c90cd45326
usmangeek <usman@galaniprojects.de>
2022-03-03 20:40:28 +0100

    Added new file with name change

--- src/Components/VerifierComponent.tsx
+++ src/Components/VerifierComponent.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import { ImportFiles } from './Verifyer/ImportFiles'
+import { ImportFiles } from './Verifier/ImportFiles'
 import { selectFile, selectFilename } from '../Features/Signer/FileSlice'
 import { useAppSelector } from '../app/hooks'
 import { FileNameList } from './FileNameList'
 import { ZippedFile } from './ZippedFile'
-import { VerifierFileList } from './Verifyer/VerifierFileList'
+import { VerifierFileList } from './Verifier/VerifierFileList'
 import CenterRightBubble from '../ImageAssets/CenterRightBubble.svg'
 import CenterLeftBubble from '../ImageAssets/CenterLeftBubble.svg'
 import { EmptyFilesList } from './EmptyFilesList'
+import { selectJwsSignStatus } from '../Features/Signer/VerifyJwsSlice'
 
-export const VerifyerComponent = () => {
+export const VerifierComponent = () => {
   const fileName = useAppSelector(selectFilename)
   const file = useAppSelector(selectFile)
+  const jwsStatus = useAppSelector(selectJwsSignStatus)
 
   return (
     <div className="bg-mid-body w-screen overflow-y-hidden overflow-x-hidden relative bottom-0 small-device:pl-[15px] small-device:pr-[15px]">
       <ImportFiles />
       <div
         className={`overflow-y-auto relative flex-col -space-y-1 mx-auto max-w-[766px] min-h-[211px] max-h-[900px]
-         bg-[#ddf0ff80] border-solid border-[#517ca240] border-t-[1px] border-l-[1px] border-r-[1px]`}
+         bg-[#ddf0ff80] border-solid border-[#517ca240]   border-t-[1px] border-l-[1px] border-r-[1px]`}
       >
         {file.length >= 0 && fileName.length === 0 && (
           <span className="absolute top-4 left-10 phone:left-2 text-[#2a223180] font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px] w-[34px]">
