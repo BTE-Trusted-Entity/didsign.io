9d4f546f2e886c44d35e17a7e9c3b85376a12b77
usmangeek <usman@galaniprojects.de>
2022-03-03 20:36:34 +0100

    Added animation components

---
+++ src/Components/Animations.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../Styles/animation.css'
+
+export const SlowAnimation = () => {
+  return (
+    <div>
+      <div className="container bg-[#3e6e99]">
+        <div className="gradient"></div>
+        <div className="circle circle1"></div>
+      </div>
+    </div>
+  )
+}
+export const FastAnimation = () => {
+  return (
+    <div>
+      <div className="container bg-[#11A770]">
+        <div className="gradient"></div>
+        <div className="circle circle1"></div>
+        <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
+        <div className="circle circle4"></div>
+        <div className="circle circle5"></div>
+      </div>
+    </div>
+  )
+}
