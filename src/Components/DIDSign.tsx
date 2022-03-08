1ac1965d1ec0b4b684c944b634087df21803a156
usmangeek <usman@galaniprojects.de>
2022-03-08 09:51:27 +0100

    Added dim background

--- src/Components/DIDSign.tsx
+++ src/Components/DIDSign.tsx
@@ -11,12 +11,21 @@ import { BottomSectionVerifyer } from './Verifier/BottomSectionVerifyer'
 import BottomLeftBubble from '../ImageAssets/BottomLeftBubble.svg'
 
 import BottomRightBubble from '../ImageAssets/BottomRightBubble.svg'
+import { selectPopup } from '../Features/Signer/PopupSlice'
 
 export const DIDSign = () => {
   const userIsSigner = useAppSelector(selectUser)
+  const popup = useAppSelector(selectPopup)
 
   return (
-    <div className="relative min-h-screen w-screen overflow-y-auto bg-bottom-body overflow-x-hidden flex flex-col">
+    <div
+      className={`relative min-h-screen w-screen overflow-y-auto bg-bottom-body overflow-x-hidden flex flex-col ${
+        popup && 'overflow-y-hidden'
+      }`}
+    >
+      {popup && (
+        <div className="bg-black opacity-70 absolute w-full h-full z-30"></div>
+      )}
       <Header />
       <SignVerifyBtn />
       {userIsSigner ? <SignerComponent /> : <VerifierComponent />}
