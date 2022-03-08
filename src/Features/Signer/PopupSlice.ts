9e73969c58c10f772bea168fbb90f8603c675e1c
usmangeek <usman@galaniprojects.de>
2022-03-08 09:58:46 +0100

    Added popup reducer

---
+++ src/Features/Signer/PopupSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+
+interface Popup {
+  show: boolean
+}
+
+// Define the initial state using that type
+const initialState: Popup = {
+  show: false,
+}
+export const popupSlice = createSlice({
+  name: 'popup',
+  initialState,
+  reducers: {
+    showPopup: (state, action: PayloadAction<boolean>) => {
+      return {
+        ...state,
+        show: action.payload,
+      }
+    },
+  },
+})
+
+export const { showPopup } = popupSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectPopup = (state: RootState) => state.popup.show
+
+export default popupSlice.reducer
