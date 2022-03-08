552d66b2a429a342f48944282eae8cbf8b77a820
usmangeek <usman@galaniprojects.de>
2022-03-08 21:54:57 +0100

    Fixed namings

--- src/Components/VerifierComponent.tsx
+++ src/Components/VerifierComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ImportFiles } from './Verifier/ImportFiles'
 import { selectFile, selectFilename } from '../Features/Signer/FileSlice'
 import { useAppSelector } from '../app/hooks'
-import { FileNameList } from './FileNameList'
+import { ZippedFilesList } from './ZippedFilesList'
 import { ZippedFile } from './ZippedFile'
 import { VerifierFileList } from './Verifier/VerifierFileList'
 import CenterRightBubble from '../ImageAssets/CenterRightBubble.svg'
@@ -10,8 +10,8 @@ import CenterLeftBubble from '../ImageAssets/CenterLeftBubble.svg'
 import { EmptyFilesList } from './EmptyFilesList'
 
 export const VerifierComponent = () => {
-  const fileName = useAppSelector(selectFilename)
-  const file = useAppSelector(selectFile)
+  const zippedFiles = useAppSelector(selectFilename)
+  const files = useAppSelector(selectFile)
 
   return (
     <div className="bg-mid-body w-screen overflow-y-hidden overflow-x-hidden relative bottom-0 small-device:pl-[15px] small-device:pr-[15px]">
@@ -20,15 +20,15 @@ export const VerifierComponent = () => {
         className={`overflow-y-auto relative flex-col -space-y-1 mx-auto max-w-[766px] min-h-[211px] max-h-[900px]
          bg-[#ddf0ff80] border-solid border-[#517ca240]   border-t-[1px] border-l-[1px] border-r-[1px]`}
       >
-        {file.length >= 0 && fileName.length === 0 && (
+        {files.length >= 0 && zippedFiles.length === 0 && (
           <span className="absolute top-4 left-10 phone:left-2 text-[#2a223180] font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px] w-[34px]">
             Files
           </span>
         )}
         <ZippedFile />
-        {fileName.length > 0 && <FileNameList />}
-        {fileName.length === 0 && file.length > 0 && <VerifierFileList />}
-        {file.length === 0 && <EmptyFilesList />}
+        {zippedFiles.length > 0 && <ZippedFilesList />}
+        {zippedFiles.length === 0 && files.length > 0 && <VerifierFileList />}
+        {files.length === 0 && <EmptyFilesList />}
       </div>
       <img
         src={CenterLeftBubble}
