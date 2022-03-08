48050f07652856a3ce35b9ea2a30ac1e715e16fa
usmangeek <usman@galaniprojects.de>
2022-03-08 09:51:49 +0100

    Removed Document.getElement

--- src/Components/SignBtn.tsx
+++ src/Components/SignBtn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 import { updateSign, updateDID } from '../Features/Signer/SignatureSlice'
 import { openSporan, generateJWS } from '../Utils/sign-helpers'
@@ -9,14 +9,19 @@ import InfoIcon from '../ImageAssets/iconBIG_info.svg'
 import AttentionIcon from '../ImageAssets/iconBIG_attention.svg'
 import CenterLeftBubble from '../ImageAssets/CenterLeftBubble.svg'
 import info from '../ImageAssets/icon_info.svg'
+import { showPopup } from '../Features/Signer/PopupSlice'
 
 export const SignBtn = () => {
-  const [signStatus, setSignStatus] = useState<boolean | 'Default'>('Default')
+  const [signStatus, setSignStatus] = useState<
+    boolean | 'Default' | 'No Sporran'
+  >('Default')
   const [popupIcon, setPopupIcon] = useState<string>(InfoIcon)
+  const sporranPopup = useRef<null | HTMLDivElement>(null)
+
   const ButtonDisabled = () => {
     return (
       <button
-        className="font-['Overpass'] text-[14px] leading-[16px]  tracking-[0.1px] text-[#ffffff80] text-center w-[160px] h-[30px] mt-4 mb-4 rounded-md 
+        className="font-['Overpass'] text-[14px] leading-[16px] pointer-events-none  tracking-[0.1px] text-[#ffffff80] text-center w-[160px] h-[30px] mt-4 mb-4 rounded-md 
                 my-auto bg-[#718BA3] shadow-md"
         onClick={handleChange}
       >
@@ -45,16 +50,8 @@ export const SignBtn = () => {
       return
     }
 
-    ;(
-      document.getElementById('sporran-popup') as HTMLDivElement
-    ).classList.remove('invisible')
-    ;(
-      document.getElementById('sign-component') as HTMLDivElement
-    ).classList.add('blur-sm')
-    ;(
-      document.getElementById('sign-component') as HTMLDivElement
-    ).style.pointerEvents = 'none'
-    document.body.style.overflowY = 'hidden'
+    dispatch(showPopup(true))
+    sporranPopup.current?.classList.remove('invisible')
     openSporan(await finalHash)
       .then(async (response) => {
         dispatch(updateSign(response.signature))
@@ -63,8 +60,6 @@ export const SignBtn = () => {
           keyID: response.keyID,
           signature: response.signature,
         }
-        document.body.style.overflowY = 'scroll'
-
         const jws = generateJWS(signature, await finalHash)
         const signedDoc: SignDoc = { hashes: hashes, jws: jws }
         const blob = new Blob([JSON.stringify(signedDoc)], {
@@ -72,38 +67,32 @@ export const SignBtn = () => {
         })
         const newFile = new File([blob], 'signature.didsign')
         dispatch(addFileTop(newFile))
-        ;(
-          document.getElementById('sign-component') as HTMLDivElement
-        ).classList.remove('blur-sm')
       })
 
       .catch(() => {
-        setPopupIcon(AttentionIcon)
-        setSignStatus(false)
+        if (window.kilt.sporran == undefined) {
+          setSignStatus('No Sporran')
+
+          setPopupIcon(InfoIcon)
+        } else {
+          setPopupIcon(AttentionIcon)
+
+          setSignStatus(false)
+        }
       })
   }
   const handleDismiss = () => {
-    document.body.style.overflowY = 'scroll'
-    ;(
-      document.getElementById('sign-component') as HTMLDivElement
-    ).style.pointerEvents = 'visible'
+    document.body.style.overflowY = 'auto'
+    dispatch(showPopup(false))
     setPopupIcon(InfoIcon)
     setSignStatus('Default')
-    ;(document.getElementById('sporran-popup') as HTMLDivElement).classList.add(
-      'invisible'
-    )
-    ;(
-      document.getElementById('sign-component') as HTMLDivElement
-    ).classList.remove('blur-sm')
-    ;(
-      document.getElementById('sign-btn') as HTMLButtonElement
-    ).classList.remove('blur-sm')
+    sporranPopup.current?.classList.add('invisible')
   }
   return (
     <div>
       <div
-        id="sporran-popup"
-        className="invisible fixed w-[24%] h-[fit] bg-mid-body shadow-2xl rounded-md left-1/2 top-1/4 mt-[3%] -ml-[12%]"
+        ref={sporranPopup}
+        className="invisible z-40 fixed max-w-[400px] phone:w-[300px] mx-auto h-[fit] bg-mid-body shadow-2xl rounded-[15px] left-1/2 top-1/4 mt-[3%] -ml-[200px] phone:-ml-[150px] phone:overflow-y-scroll"
       >
         <div className="flex relative flex-col w-full h-full items-center mt-4 space-y-5 pl-4 pr-4 pb-4 ">
           <img
@@ -122,19 +111,29 @@ export const SignBtn = () => {
               Sign Error
             </span>
           )}
-          {signStatus === 'Default' ? (
+          {signStatus === 'Default' && (
             <span className="font-['Overpass'] -tracking-tigher text-[#2A2231] text-[16px] pl-4 pr-4 text-justify">
               Please wait for your wallet extension to open and sign the
               transaction there.
             </span>
-          ) : (
+          )}{' '}
+          {signStatus === false && (
             <span className="font-['Overpass'] -tracking-tigher text-[#2A2231] text-[16px] pl-4 pr-4 text-justify">
               It looks like error occured while signing. Please try again.
             </span>
           )}
+          {signStatus === 'No Sporran' && (
+            <span className="font-['Overpass'] -tracking-tigher text-[#2A2231] text-[16px] pl-4 pr-4 text-justify">
+              To sign your files with DIDsign you need an on-chain DID in a
+              wallet that supports it. We recommend the Sporran, a browser
+              extension available for Google Chrome and Firefox. Any other
+              wallet supporting on-chain signing on the KILT blockchain can also
+              be used.
+            </span>
+          )}
           <button
             onClick={handleDismiss}
-            className="font-['Overpass'] rounded-md w-1/3 text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[25px] bg-[#3E6E99] text-white"
+            className="font-['Overpass'] rounded-[8px] w-[100px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[25px] bg-[#3E6E99] text-white"
           >
             DISMISS
           </button>
@@ -143,7 +142,7 @@ export const SignBtn = () => {
 
       <div
         id="sign-btn"
-        className="bg-[#ddf0ff80] border-[#517ca240] border-[1px] space-x-2 rounded-b-[15px] mt-0  mx-auto max-w-[766px] flex items-center h-[6rem] justify-center mb-2 shadow-md"
+        className="bg-[#ddf0ff80] border-[#517ca240] border-[1px] space-x-2 rounded-b-[15px] mt-0  mx-auto max-w-[766px] flex items-center h-[6rem] justify-center mb-4 shadow-md"
       >
         {hashes.length == 0 ? <ButtonDisabled /> : <ButtonEnabled />}
         <img src={info}></img>
