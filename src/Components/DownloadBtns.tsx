bf91befed810c2a92aab55ddc305bf39337378eb
usmangeek <usman@galaniprojects.de>
2022-03-10 13:50:32 +0100

    Update:Zipping Button

--- src/Components/DownloadBtns.tsx
+++ src/Components/DownloadBtns.tsx
@@ -57,12 +57,14 @@ export const DownloadBtns = () => {
   return (
     <div className="bg-light-blue bg-opacity-80 border-solid border-[#517ca240] border-[1px] mx-auto flex flex-col items-center justify-center rounded-b-[15px] max-w-[766px] pt-4 pr-4 shadow-sm mb-8 space-y-4 relative">
       <div className="flex space-x-8 justify-center items-center ">
-        <span className="w-[110px] text-right text-[14px] font-['Overpass' absolute left-[180px] phone:invisible text-dark-purple">
-          now
-        </span>
+        {!showLoader && (
+          <span className="w-[110px] text-right text-[14px] font-['Overpass' absolute left-[180px] phone:invisible text-dark-purple">
+            now
+          </span>
+        )}
         <button
           className={`w-[160px] h-[30px] text-[14px] leading-[16px] relative tracking-[0.1px]  font-['Overpass'] bg-medium-blue text-white rounded-[8px] ${
-            showLoader && 'pointer-events-none'
+            showLoader && 'pointer-events-none mb-6'
           }`}
           onClick={handleZip}
         >
@@ -73,17 +75,19 @@ export const DownloadBtns = () => {
           )}
         </button>
       </div>
-      <div className="flex space-x-8 justify-start items-center pb-4">
-        <span className="text-[14px] leading-[16px] tracking-[0.1px] w-[110px] font-['Overpass'] text-right absolute left-[185px] phone:invisible text-dark-purple">
-          or only download
-        </span>
-        <button
-          className="w-[130px] h-[22px] font-['Overpass'] bg-medium-blue text-white text-[12px] leading-[13px] tracking-[0.09px] rounded-[6px]"
-          onClick={handleDownloadSign}
-        >
-          <span>SIGNATURE</span>
-        </button>
-      </div>
+      {!showLoader && (
+        <div className="flex space-x-8 justify-start items-center pb-4">
+          <span className="text-[14px] leading-[16px] tracking-[0.1px] w-[110px] font-['Overpass'] text-right absolute left-[185px] phone:invisible text-dark-purple">
+            or only download
+          </span>
+          <button
+            className="w-[130px] h-[22px] font-['Overpass'] bg-medium-blue text-white text-[12px] leading-[13px] tracking-[0.09px] rounded-[6px]"
+            onClick={handleDownloadSign}
+          >
+            <span>SIGNATURE</span>
+          </button>
+        </div>
+      )}
       <button className="absolute -right-1 -bottom-1" onClick={handleStartOver}>
         <img className="h-12 w-12" src={BtnStartOver} />
       </button>
