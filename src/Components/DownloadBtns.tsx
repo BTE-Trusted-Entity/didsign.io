e2a086502b9dcda8c6d079398ec89a1ed0a0f87a
usmangeek <usman@galaniprojects.de>
2022-03-10 19:55:48 +0100

    Feat: Zip Progress bar

--- src/Components/DownloadBtns.tsx
+++ src/Components/DownloadBtns.tsx
@@ -10,16 +10,17 @@ import {
   selectDid,
   selectSign,
 } from '../Features/Signer/SignatureSlice'
-import { generateJWS, generateZipFile } from '../Utils/sign-helpers'
+import { generateJWS } from '../Utils/sign-helpers'
 import { Signature, SignDoc } from '../Utils/types'
 import { saveAs } from 'file-saver'
 import {
   clearAll,
   clearFileName,
+  IBuffer,
   selectBuffer,
 } from '../Features/Signer/FileSlice'
 import BtnStartOver from '../ImageAssets/button_start_over_NEW.svg'
-import loader from '../ImageAssets/oval.svg'
+import JSZip from 'jszip'
 
 export const DownloadBtns = () => {
   const sign = useAppSelector(selectSign)
@@ -28,10 +29,29 @@ export const DownloadBtns = () => {
   const hashes = useAppSelector(selectHash)
   const buffers = useAppSelector(selectBuffer)
   const [showLoader, setShowLoader] = useState<boolean>(false)
+  const [progress, setProgress] = useState<string>('0')
 
   const signature: Signature = { keyID: did, signature: sign }
 
   const dispatch = useAppDispatch()
+  const generateZipFile = async (buffers: IBuffer[]) => {
+    const zip = new JSZip()
+    buffers.map((buffer) => zip.file(buffer.name, buffer.buffer))
+    const content = await zip.generateAsync(
+      {
+        type: 'blob',
+        compression: 'STORE',
+        streamFiles: true,
+        compressionOptions: {
+          level: 1,
+        },
+      },
+      function updateCallback(metadata) {
+        setProgress(metadata.percent.toFixed(0))
+      }
+    )
+    saveAs(content, 'DIDsign-files.zip')
+  }
 
   const handleDownloadSign = async () => {
     const jws = generateJWS(signature, await finalHash)
@@ -54,27 +74,36 @@ export const DownloadBtns = () => {
     dispatch(clearHash())
     dispatch(clearFileName())
   }
+
   return (
     <div className="bg-light-blue bg-opacity-80 border-solid border-[#517ca240] border-[1px] mx-auto flex flex-col items-center justify-center rounded-b-[15px] max-w-[766px] pt-4 pr-4 shadow-sm mb-8 space-y-4 relative">
       <div className="flex space-x-8 justify-center items-center ">
-        {!showLoader && (
-          <span className="w-[110px] text-right text-[14px] font-['Overpass' absolute left-[180px] phone:invisible text-dark-purple">
-            now
-          </span>
-        )}
+        <span className="w-[110px] text-right text-[14px] font-['Overpass' absolute left-[180px] phone:invisible text-dark-purple">
+          now
+        </span>
+
         <button
           className={`w-[160px] h-[30px] text-[14px] leading-[16px] relative tracking-[0.1px]  font-['Overpass'] bg-medium-blue text-white rounded-[8px] ${
-            showLoader && 'pointer-events-none mb-6'
+            showLoader && 'pointer-events-none'
           }`}
           onClick={handleZip}
         >
           <span>{showLoader ? 'ZIPPING' : 'ZIP ALL FILES'}</span>
-
-          {showLoader && (
-            <img className={`absolute right-4 h-1/2 top-[6px]`} src={loader} />
-          )}
         </button>
       </div>
+      {showLoader && (
+        <div className="flex flex-col space-y-1 justify-center items-end w-[190px] pb-3">
+          <div className="w-[160px] bg-white h-[4px] rounded-[10px]">
+            <div
+              className=" bg-medium-blue h-full rounded-[10px]"
+              style={{ width: progress + '%' }}
+            ></div>
+          </div>
+          <span className="w-[160px] text-center text-[12px] h-[10px] leading-[16px] tracking-[0.1px] ont-['Overpass'] text-dark-purple">
+            {progress + '%'}
+          </span>
+        </div>
+      )}
       {!showLoader && (
         <div className="flex space-x-8 justify-start items-center pb-4">
           <span className="text-[14px] leading-[16px] tracking-[0.1px] w-[110px] font-['Overpass'] text-right absolute left-[185px] phone:invisible text-dark-purple">
--- src/Utils/sign-helpers.ts
+++ src/Utils/sign-helpers.ts
@@ -2,10 +2,7 @@ import * as hasher from 'multiformats/hashes/hasher'
 import { sha256 } from 'js-sha256'
 import { base16 } from 'multiformats/bases/base16'
 import * as json from 'multiformats/codecs/json'
-import JSZip from 'jszip'
-import { saveAs } from 'file-saver'
 import { Signature } from './types'
-import { IBuffer } from '../Features/Signer/FileSlice'
 
 export const sha56 = hasher.from({
   name: 'sha2-256',
@@ -30,20 +27,6 @@ export const createHashFromHashArray = async (
   const sortedHash = [...hashArray].sort()
   const asJson = json.encode(sortedHash)
   return await createHash(asJson)
-}
-
-export const generateZipFile = async (buffers: IBuffer[]) => {
-  const zip = new JSZip()
-  buffers.map((buffer) => zip.file(buffer.name, buffer.buffer))
-  const content = await zip.generateAsync({
-    type: 'blob',
-    compression: 'STORE',
-    streamFiles: true,
-    compressionOptions: {
-      level: 1,
-    },
-  })
-  saveAs(content, 'DIDsign-files.zip')
 }
 
 export const openSporan = async (finalHash: string): Promise<Signature> => {
