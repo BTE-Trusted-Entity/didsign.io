5e7f0a11a626d964ae59864ba3387afbba6ca8a4
usmangeek <usman@galaniprojects.de>
2022-04-22 12:24:07 +0200

    feat: add styled list for zip files

--- src/StyledComponents/FilesList.ts
+++ src/StyledComponents/FilesList.ts
@@ -4,9 +4,8 @@ import { colors } from './colors'
 export const StyledList = styled.div`
   display: flex;
   flex-direction: column;
-  width: 631px;
+  width: 609px;
   gap: 15px;
-  margin-right: 22px;
   margin-top: 10px;
   margin-bottom: 6px;
 `
@@ -50,4 +49,41 @@ export const FilesSeparator = styled.div`
   border: dotted 1px ${colors.darkpurple};
   margin-top: 5px;
   opacity: 0.5;
+`
+export const ZipContainer = styled.div`
+  display: flex;
+  width: 95%;
+  justify-content: flex-start;
+`
+export const ZipFileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-left: 40px;
+  margin-top: 10px;
+  justify-content: flex-start;
+  align-items: flex-end;
+`
+export const CompressedFilesWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: 0.1px;
+  font-family: Overpass;
+`
+export const ZipFileSeparator = styled.div`
+  width: 96%;
+  border: dotted 1px ${colors.darkpurple};
+  margin-top: 5px;
+  opacity: 0.5;
+`
+export const CompressedFilesList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 88%;
+  gap: 15px;
+  margin-top: 10px;
+  margin-bottom: 6px;
 `
