9de1d0fbf04aa424531ee073874c5fbef9641a01
usmangeek <usman@galaniprojects.de>
2022-04-22 12:21:22 +0200

    fix: delete when user role changes

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import TopRightBubble from '../ImageAssets/TopRightBubble.svg'
 import TopLeftBubble from '../ImageAssets/TopLeftBubble.svg'
+import SecondLeftBubble from '../ImageAssets/SecondHeaderBubble.svg'
+
 import {
   Container,
   HeaderContainer,
   HeaderLogo,
   LogoContainer,
   SecondaryHeaderContainer,
+  SecondaryLeftBubble,
   SignRoleBtn,
   SignUnderline,
   StyledHeader,
@@ -17,6 +20,14 @@ import {
 } from '../StyledComponents/Header'
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 import { selectUserRole, updateRole } from '../Features/Signer/UserSlice'
+import { clearSign } from '../Features/Signer/SignatureSlice'
+import { clearAll, clearFileName } from '../Features/Signer/FileSlice'
+import { clearHash } from '../Features/Signer/hashSlice'
+import {
+  clearEndpoint,
+  clearFileStatuses,
+} from '../Features/Signer/EndpointSlice'
+import { clearJWS } from '../Features/Signer/VerifyJwsSlice'
 
 export const Header = () => {
   return (
@@ -43,9 +54,19 @@ const SecondaryHeader = () => {
   const dispatch = useAppDispatch()
   const handleVerifier = () => {
     dispatch(updateRole(false))
+    dispatch(clearSign())
+    dispatch(clearAll())
+    dispatch(clearHash())
   }
   const handleSigner = () => {
     dispatch(updateRole(true))
+    dispatch(clearSign())
+    dispatch(clearAll())
+    dispatch(clearHash())
+    dispatch(clearFileName())
+    dispatch(clearEndpoint())
+    dispatch(clearJWS())
+    dispatch(clearFileStatuses())
   }
   return (
     <SecondaryHeaderContainer>
@@ -69,6 +90,7 @@ const SecondaryHeader = () => {
           <VerifyUnderline isSelectedRole={!userRoleSigner} />
         </SignRoleBtn>
       </Container>
+      <SecondaryLeftBubble src={SecondLeftBubble} />
     </SecondaryHeaderContainer>
   )
 }
