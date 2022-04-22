11490227d4c1f127e8cf8756e67846a20dd73ed5
usmangeek <usman@galaniprojects.de>
2022-04-22 16:56:23 +0200

    refactor: use styled components

--- src/Components/Popups.tsx
+++ src/Components/Popups.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import AttentionIcon from '../ImageAssets/iconBIG_attention.svg'
 import InfoIcon from '../ImageAssets/iconBIG_info.svg'
-import CenterLeftBubble from '../ImageAssets/CenterLeftBubble.svg'
 import { useAppDispatch } from '../app/hooks'
 import { showPopup } from '../Features/Signer/PopupSlice'
 import { updateSignStatus } from '../Features/Signer/VerifyJwsSlice'
 import { clearEndpoint } from '../Features/Signer/EndpointSlice'
 import SignatureIcon from '../ImageAssets/icon_DID.svg'
-import BTELogo from '../ImageAssets/bte_logo_black.png'
+import BTE from '../ImageAssets/bte_logo_black.png'
 import {
   Container,
   DismissBtn,
@@ -16,45 +15,18 @@ import {
   Text,
   Wrapper,
 } from '../StyledComponents/Popup'
+import {
+  BTELogo,
+  Dismiss,
+  Imprint,
+  ImprintContainer,
+  ImprintText,
+} from '../StyledComponents/Footer'
 
 interface Toggle {
   dismiss: React.MouseEventHandler<HTMLButtonElement>
 }
 
-export const SignInfoPopup = (props: Toggle) => {
-  return (
-    <div className=" z-40 text-dark-purple flex fixed max-w-[300px] mx-auto h-[fit] bg-silver-blue shadow-2xl rounded-[15px] left-1/2 phone:left-[15%] top-1/4 mt-[3%] -ml-[200px] overflow-y-auto">
-      <div className="flex relative flex-col w-full h-full items-center mt-4 space-y-3 pl-4 pr-4 mb-4 ">
-        <img
-          src={CenterLeftBubble}
-          className="absolute bottom-0 left-0 mt-auto pointer-events-none"
-        />
-        <div>
-          <img src={InfoIcon} />
-        </div>
-        <span className="font-['Overpass'] tracking-[0.13px] text-[18px] leading-[20px]">
-          Signing
-        </span>
-        <div className="flex flex-col space-y-5  pl-4 pr-4 text-left pb-5 phone:pb-0 font-['Overpass'] leading-[16px] tracking-[0.13px] text-[14px]">
-          <p>
-            In order to successfully sign your files with DIDsign, make sure to
-            have a wallet installed that has an onchain DID.
-          </p>
-          <p>
-            We recommend to use Sporran, which is a browser extension available
-            for Google Chrome and Mozilla Firefox.
-          </p>
-        </div>
-        <button
-          onClick={props.dismiss}
-          className="font-['Overpass'] whitespace-nowrap rounded-[8px] w-[130px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[22px] bg-medium-blue text-white"
-        >
-          OK
-        </button>
-      </div>
-    </div>
-  )
-}
 export const MultipleSignPopup = () => {
   const dispatch = useAppDispatch()
   const handleDismiss = () => {
@@ -63,185 +35,108 @@ export const MultipleSignPopup = () => {
     dispatch(updateSignStatus('Not Checked'))
   }
   return (
-    <div className=" z-40 text-dark-purple fixed max-w-[400px] phone:w-[300px] mx-auto h-[fit] bg-silver-blue shadow-2xl rounded-[15px] left-1/2 top-1/4 mt-[3%] -ml-[200px] phone:-ml-[150px] phone:overflow-y-scroll">
-      <div className="flex relative flex-col w-full h-full items-center mt-4 space-y-5 pl-4 pr-4 pb-4 ">
-        <img
-          src={CenterLeftBubble}
-          className="absolute bottom-0 left-0 mt-auto pointer-events-none"
-        />
-        <div>
+    <Container>
+      <StyledPopup>
+        <Wrapper>
           <img src={AttentionIcon} />
-        </div>
-        <span className="font-['Overpass'] tracking-wide text-lg ">
-          Verification Error
-        </span>
-
-        <p className="font-['Overpass'] tracking-[0.13px]  text-[16px] pl-4 pr-4 text-left">
-          Multiple signature files found. Please import only one signature file.
-        </p>
-
-        <button
-          onClick={handleDismiss}
-          className="font-['Overpass'] rounded-[8px] w-[100px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[25px] bg-medium-blue text-white"
-        >
-          DISMISS
-        </button>
-      </div>
-    </div>
+          <Heading>Verification Error</Heading>
+          <Text>
+            {' '}
+            Multiple signature files found. Please import only one signature
+            file.
+          </Text>
+          <DismissBtn onClick={() => handleDismiss()}>DISMISS</DismissBtn>
+        </Wrapper>
+      </StyledPopup>
+    </Container>
   )
 }
 export const SignFileInfoPopup = (props: Toggle) => {
   return (
-    <div className=" z-40 flex fixed text-dark-purple max-w-[360px] mx-auto h-[400px] bg-silver-blue shadow-2xl rounded-[15px] left-3/4 phone:overflow-y-scroll top-[10%] mt-[3%] -ml-[180px]">
-      <div className="flex relative flex-col w-full h-full items-center mt-2 space-y-4 pl-4 pr-4 mb-4 ">
-        <img
-          src={CenterLeftBubble}
-          className="absolute bottom-0 left-0 mt-auto pointer-events-none"
-        />
-        <div>
+    <Container>
+      <StyledPopup>
+        <Wrapper>
           <img src={InfoIcon} />
-        </div>
-        <div className="flex space-x-1 pt-4">
-          <img src={SignatureIcon} />
-          <span className="font-['Overpass'] tracking-[0.13px] text-[18px] leading-[20px] ">
+          <Heading>
+            <img src={SignatureIcon} />
             Signature
-          </span>
-        </div>
-        <div className="flex flex-col space-y-5  pl-4 pr-4 text-left pt-2 pb-2 font-['Overpass'] leading-[16px] tracking-[0.13px]  text-[14px]">
-          <p>
+          </Heading>
+          <Text>
             Your files have been signed and your DIDsign signature has been
             added successfully.
-          </p>
-          <p>
-            The receiver of your documents needs to get your signature together
-            with the set of signed files in order to get the verfification.
-          </p>
-          <p>
-            The easiest way to proceed is to zip all files into one archive.
-          </p>
-        </div>
-        <button
-          onClick={props.dismiss}
-          className="font-['Overpass'] whitespace-nowrap rounded-[8px] w-[130px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[22px] bg-medium-blue text-white"
-        >
-          OK
-        </button>
-      </div>
-    </div>
+            <br /> The receiver of your documents needs to get your signature
+            together with the set of signed files in order to get the
+            verification. <br /> The easiest way to proceed is to zip all files
+            into one archive.
+          </Text>
+          <Dismiss onClick={props.dismiss}>OK</Dismiss>
+        </Wrapper>
+      </StyledPopup>
+    </Container>
   )
 }
 export const SigningMultipleDidFiles = (props: Toggle) => {
   return (
-    <div className=" z-40 text-dark-purple fixed w-[300px] phone:w-[300px] mx-auto h-[fit] bg-silver-blue shadow-2xl rounded-[15px] left-1/2 top-1/4 mt-[3%] -ml-[150px] phone:-ml-[150px] phone:overflow-y-scroll">
-      <div className="flex relative flex-col w-full h-full items-center mt-4 space-y-5 pl-4 pr-4 pb-4 ">
-        <img
-          src={CenterLeftBubble}
-          className="absolute bottom-0 left-0 mt-auto pointer-events-none"
-        />
-        <div>
+    <Container>
+      <StyledPopup>
+        <Wrapper>
           <img src={AttentionIcon} />
-        </div>
-        <span className="font-['Overpass'] tracking-wide text-lg ">
-          Sign Error
-        </span>
-
-        <p className="font-['Overpass'] tracking-[0.13px]  text-[16px] pl-4 pr-4 text-left">
-          Signing of signature file is not allowed.
-        </p>
-
-        <button
-          onClick={props.dismiss}
-          className="font-['Overpass'] rounded-[8px] w-[100px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[25px] bg-medium-blue text-white"
-        >
-          DISMISS
-        </button>
-      </div>
-    </div>
+          <Heading>Sign Error</Heading>
+          <Text> Signing of signature file is not allowed.</Text>
+          <DismissBtn onClick={props.dismiss}>DISMISS</DismissBtn>
+        </Wrapper>
+      </StyledPopup>
+    </Container>
   )
 }
 export const SigningDuplicateFiles = (props: Toggle) => {
   return (
-    <div className=" z-40 text-dark-purple fixed w-[300px] phone:w-[300px] mx-auto h-[fit] bg-silver-blue shadow-2xl rounded-[15px] left-1/2 top-1/4 mt-[3%] -ml-[150px] phone:-ml-[150px] phone:overflow-y-scroll">
-      <div className="flex relative flex-col w-full h-full items-center mt-4 space-y-5 pl-4 pr-4 pb-4 ">
-        <img
-          src={CenterLeftBubble}
-          className="absolute bottom-0 left-0 mt-auto pointer-events-none"
-        />
-        <div>
+    <Container>
+      <StyledPopup>
+        <Wrapper>
           <img src={AttentionIcon} />
-        </div>
-        <span className="font-['Overpass'] tracking-wide text-lg ">
-          Sign Error
-        </span>
-
-        <p className="font-['Overpass'] tracking-[0.13px]  text-[16px] pl-4 pr-4 text-left">
-          Duplicate files found. Please include each file only once
-        </p>
-
-        <button
-          onClick={props.dismiss}
-          className="font-['Overpass'] rounded-[8px] w-[100px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[25px] bg-medium-blue text-white"
-        >
-          DISMISS
-        </button>
-      </div>
-    </div>
+          <Heading>Sign Error</Heading>
+          <Text>Duplicate files found. Please include each file only once</Text>
+          <DismissBtn onClick={props.dismiss}>DISMISS</DismissBtn>
+        </Wrapper>
+      </StyledPopup>
+    </Container>
   )
 }
 export const ImprintPopup = (props: Toggle) => {
   return (
-    <div className=" z-40 text-dark-purple max-w-[500px] flex mx-auto h-[fit] bg-silver-blue shadow-2xl rounded-[15px] absolute bottom-24 overflow-y-auto ml-[15px] mr-[15px]">
-      <div className="flex relative flex-col w-full h-full items-center mt-4 space-y-5 pl-4 pr-4 pb-4 ">
-        <img
-          src={CenterLeftBubble}
-          className="absolute bottom-0 left-0 mt-auto pointer-events-none"
-        />
-        <div>
-          <img className="w-[101px] h-[80px]" src={BTELogo} />
-        </div>
-        <div className="flex flex-col flex-wrap space-y-1 overflow-wrap break-words  pl-[10px] pr-[10px] text-left pt-2 pb-2 font-['Overpass'] leading-[16px] tracking-[0.13px]  text-[14px]">
-          <span>Imprint</span>
-          <span>B.T.E. BOTLabs Trusted Entity GmbH</span>
-          <span>Keithstraße 2-4</span>
-          <span>10787 Berlin, Germany</span>
-          <span>
-            Germany Commercial Court: Amtsgericht Charlottenburg in Berlin
-          </span>
-          <span>Registration Number: HRB 231219B</span>
-          <span>VAT No: DE 346528612</span>
-          <span>Managing Director: Ingo Rübe</span>
-          <span>
-            Contact:{' '}
-            <a
-              href="mailto:info@botlabs.org"
-              className="text-medium-blue font-bold hover:underline"
-            >
-              info@botlabs.org
-            </a>
-          </span>
-          <span>
-            Or go to{' '}
-            <a
-              href="https://support.kilt.io/support/home"
-              className="text-medium-blue font-bold hover:underline"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {' '}
-              Tech Support{' '}
-            </a>{' '}
-            and click on “Contact Us”
-          </span>
-          <span>Requirements according to § 5 TMG (Germany)</span>
-        </div>
-        <button
-          onClick={props.dismiss}
-          className="font-['Overpass'] rounded-[8px] w-[160px] text-[12px] leading-[12px]  tracking-[0.1px] pl-4 pr-4 text-center h-[25px] bg-medium-blue text-white"
-        >
-          OK
-        </button>
-      </div>
-    </div>
+    <Imprint>
+      <ImprintContainer>
+        <BTELogo src={BTE} />
+        <ImprintText setMargin>Imprint</ImprintText>
+        <ImprintText>B.T.E. BOTLabs Trusted Entity GmbH</ImprintText>
+        <ImprintText>Keithstraße 2-4</ImprintText>
+        <ImprintText>10787 Berlin, Germany</ImprintText>
+        <ImprintText>
+          Germany Commercial Court: Amtsgericht Charlottenburg in Berlin
+        </ImprintText>
+        <ImprintText>Registration Number: HRB 231219B</ImprintText>
+        <ImprintText>VAT No: DE 346528612</ImprintText>
+        <ImprintText>Managing Director: Ingo Rübe</ImprintText>
+        <ImprintText>
+          Contact: <a href="mailto:info@botlabs.org">info@botlabs.org</a>
+        </ImprintText>
+        <ImprintText setMargin>
+          Or go to{' '}
+          <a
+            href="https://support.kilt.io/support/home"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {' '}
+            Tech Support{' '}
+          </a>{' '}
+          and click on “Contact Us”
+        </ImprintText>
+        <ImprintText>Requirements according to § 5 TMG (Germany)</ImprintText>
+        <Dismiss onClick={props.dismiss}>DISMISS</Dismiss>
+      </ImprintContainer>
+    </Imprint>
   )
 }
 export const SignPopup = (props: Toggle) => {
--- src/Components/Verifier/JWSErrorsComponent.tsx
+++ src/Components/Verifier/JWSErrorsComponent.tsx
@@ -1,56 +1,44 @@
 import React from 'react'
 import { useAppSelector } from '../../app/hooks'
-import { fileStatus } from '../../Features/Signer/EndpointSlice'
 import { selectJwsSignStatus } from '../../Features/Signer/VerifyJwsSlice'
 import AttentionIcon from '../../ImageAssets/icon_attention.svg'
+import {
+  DidDocContainer,
+  TextWrapper,
+  Title,
+  VerificationIcon,
+  VerifiedText,
+} from '../../StyledComponents/DidDocument'
+import { Text } from '../../StyledComponents/DidDocument'
 
 export const JWSErrorsComponent = () => {
   const jwsStatus = useAppSelector(selectJwsSignStatus)
-  const fileVerificationStatus = useAppSelector(fileStatus)
   if (jwsStatus === 'Multiple Sign') {
     return null
   }
-
-  const showErrorMessages = () => {
-    if (jwsStatus === 'Invalid') {
-      return (
-        <span className="phone:w-full overflow-wrap break-words w-4/5 h-[18px]  font-['Overpass'] font-bold text-[14px] leading-[22px] tracking-[0.1px]">
-          The signature does not match with the imported files. Please make sure
-          to import the correct files.
-        </span>
-      )
-    } else if (fileVerificationStatus.includes(false)) {
-      return (
-        <span className="phone:w-full overflow-wrap break-words w-4/5 h-[18px]  font-['Overpass'] font-bold text-[14px] leading-[22px] tracking-[0.1px]">
-          The signature does not match with the imported files. Please make sure
-          to import the correct files.
-        </span>
-      )
-    } else if (jwsStatus === 'Corrupted') {
-      return (
-        <span className="phone:w-full overflow-wrap break-words w-4/5 h-[18px] font-['Overpass'] font-bold text-[14px] leading-[22px] tracking-[0.1px]">
-          The signature file is corrupted. Please make sure to import the
-          correct signature file.
-        </span>
-      )
-    }
-  }
-
   return (
-    <div className="flex flex-col w-[90%] mx-auto ml-3 text-attention-orange  mt-10 mb-20 space-y-[40px] phone:space-y-[20px]">
-      <div className="flex w-full items-center justify-start space-x-[10px]">
-        <p className="  font-['Overpass'] text-[16px] leading-[17px] tracking-[0.11px] w-[82px]">
-          Verification
-        </p>
-        <img src={AttentionIcon} />{' '}
-      </div>
-      <div className="max-w-full flex w-[698px] flex-wrap space-x-[20px] phone:space-x-0 phone:space-y-[20px] phone:pt-4">
-        <p className="font-['Overpass'] text-[16px] leading-[22px] tracking-[0.11px">
-          Attention
-        </p>
-
-        {showErrorMessages()}
-      </div>
-    </div>
+    <DidDocContainer>
+      <TextWrapper>
+        <VerifiedText error>Verification</VerifiedText>
+        <VerificationIcon>
+          <img src={AttentionIcon} />{' '}
+        </VerificationIcon>
+      </TextWrapper>
+      <TextWrapper>
+        <Title error>Attention</Title>
+        {jwsStatus === 'Corrupted' && (
+          <Text error>
+            The signature does not match with the imported files. Please make
+            sure to import the correct files.
+          </Text>
+        )}
+        {jwsStatus === 'Invalid' && (
+          <Text error>
+            The signature does not match with the imported files. Please make
+            sure to import the correct files.
+          </Text>
+        )}
+      </TextWrapper>
+    </DidDocContainer>
   )
 }
