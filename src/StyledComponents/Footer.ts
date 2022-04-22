318fef02b15e1af1abc51da7e254f46c21bd448d
usmangeek <usman@galaniprojects.de>
2022-04-22 16:58:18 +0200

    style: add imprint component

--- src/StyledComponents/Footer.ts
+++ src/StyledComponents/Footer.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 import { colors } from './colors'
 
+interface Style {
+  setMargin?: boolean
+}
 export const StyledFooter = styled.div`
   background-color: ${colors.darkpurple};
   height: 35px;
@@ -54,4 +57,60 @@ export const LogoContainer = styled.div`
   @media (max-width: 400px) {
     display: none;
   }
+`
+export const Imprint = styled.div`
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  height: 480px;
+  position: fixed;
+  bottom: 113px;
+  z-index: 40;
+`
+export const ImprintContainer = styled.div`
+  display: flex;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: ${colors.silverblue};
+  flex-direction: column;
+  word-break: break-all;
+  height: 522px;
+  max-width: 484px;
+  width: 90%;
+  justify-content: center;
+  align-items: center;
+  color: ${colors.darkpurple};
+  gap: 2px;
+  border-radius: 8px;
+`
+export const ImprintText = styled.span`
+  word-break: break-all;
+  font-size: 14px;
+  letter-spacing: 0.1px;
+  line-height: 22px;
+  margin-bottom: ${(props: Style) => props.setMargin && '15px'};
+  a {
+    color: ${colors.mediumblue};
+  }
+`
+export const Dismiss = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  letter-spacing: 0.1px;
+  line-height: 22px;
+  height: 24px;
+  width: 140px;
+  border-radius: 8px;
+  background-color: ${colors.mediumblue};
+  margin-top: 30px;
+  color: white;
+  margin-bottom: 10px;
+`
+export const BTELogo = styled.img`
+  height: 100px;
+  margin-bottom: 30px;
+  margin-top: 20px;
 `
