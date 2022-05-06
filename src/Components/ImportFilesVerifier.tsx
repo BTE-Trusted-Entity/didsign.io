aafa6cd1dad613f2871a61f25e8ce9716bd4d01c
usmangeek <usman@galaniprojects.de>
2022-05-06 11:44:42 +0200

    SK-1033:fix multiple files error

--- src/Components/ImportFilesVerifier.tsx
+++ src/Components/ImportFilesVerifier.tsx
@@ -59,7 +59,8 @@ export const ImportFilesVerifier = () => {
   const statuses = useAppSelector(fileStatus)
 
   const dispatch = useAppDispatch()
-
+  const filesArrayHasDidSign = (files: File[]) =>
+    files.filter((file) => isDidSignFile(file.name)).length > 0
   const handleZipCase = async (file: File) => {
     dispatch(updateSignStatus('Validating'))
 
@@ -74,16 +75,8 @@ export const ImportFilesVerifier = () => {
     return
   }
 
-  const handleIndividualCase = async (file: File, acceptedFiles: File[]) => {
+  const handleIndividualCase = async (file: File) => {
     let doc: SignDoc = { jws: '', hashes: [] }
-    if (
-      acceptedFiles.filter((file) => isDidSignFile(file.name)).length > 1 &&
-      isDidSignFile(file.name)
-    ) {
-      dispatch(updateSignStatus('Multiple Sign'))
-      dispatch(showPopup(true))
-      return
-    }
 
     const reader = new FileReader()
     reader.readAsArrayBuffer(file)
@@ -120,6 +113,12 @@ export const ImportFilesVerifier = () => {
   }
   const handleDrop = useCallback(
     (acceptedFiles: File[]) => {
+      if (filesArrayHasDidSign(files) && filesArrayHasDidSign(acceptedFiles)) {
+        setImportIcon(ImportIcon)
+        dispatch(updateSignStatus('Multiple Sign'))
+        dispatch(showPopup(true))
+        return
+      }
       acceptedFiles.forEach(async (file: File) => {
         setImportIcon(ImportIcon)
         if (files.length === 0) {
@@ -139,6 +138,7 @@ export const ImportFilesVerifier = () => {
               .length === 1 &&
             didSignFile.length === 1
           ) {
+            setImportIcon(ImportIcon)
             dispatch(showPopup(true))
             dispatch(updateSignStatus('Multiple Sign'))
             return
@@ -154,15 +154,7 @@ export const ImportFilesVerifier = () => {
             return
           }
         }
-        if (
-          files.filter((file) => isDidSignFile(file.name)).length >= 1 &&
-          file.name.split('.').pop() === 'didsign'
-        ) {
-          dispatch(showPopup(true))
-          dispatch(updateSignStatus('Multiple Sign'))
-          return
-        }
-        await handleIndividualCase(file, acceptedFiles)
+        await handleIndividualCase(file)
       })
     },
     [files, savedZippedFilenames]
