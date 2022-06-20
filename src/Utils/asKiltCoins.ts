0afcf0005babc60da911e2cf8b381b670d1132a3
Muhammad Usman <94175349+usmangeek@users.noreply.github.com>
2022-06-20 11:39:19 +0200

    SK-1135: fix timestamp cost (#36)

--- src/Utils/asKiltCoins.ts
+++ src/Utils/asKiltCoins.ts
@@ -2,7 +2,7 @@ import BN from 'bn.js'
 
 const KILT_POWER = 15
 
-const PRECISION = 2
+const PRECISION = 3
 const FORMAT = {
   minimumFractionDigits: PRECISION,
   maximumFractionDigits: PRECISION,
@@ -12,13 +12,12 @@ export function asKiltCoins(amount: BN): string {
   const amountString = amount.toString().padStart(16, '0')
   const whole = amountString.slice(0, -KILT_POWER)
   const fraction = amountString.slice(-KILT_POWER)
-
   const visible = fraction.slice(0, PRECISION)
-  const discarded = fraction.slice(PRECISION)
-  const roundDown = discarded.match(/^0*$/)
-  const replacement = roundDown ? '0' : '9'
 
-  const numberWithFractions = parseFloat(`${whole}.${visible}${replacement}`)
+  const numberWithFractions = parseFloat(`${whole}.${visible}`)
 
-  return numberWithFractions.toLocaleString(window.navigator.language, FORMAT)
+  return ` ≈ ${numberWithFractions.toLocaleString(
+    window.navigator.language,
+    FORMAT
+  )}`
 }
