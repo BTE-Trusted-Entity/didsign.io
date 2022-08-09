c9f5b96689139a936cb51b5b182f0b2fa519155c
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2022-08-09 15:26:04 +0200

    SK-1236-PR-workflow

--- src/Utils/global.d.ts
+++ src/Utils/global.d.ts
@@ -1,6 +1,14 @@
+import { DidResourceUri } from '@kiltprotocol/sdk-js';
+
+interface InjectedWindowProvider {
+  signWithDid: (
+    plaintext: string,
+  ) => Promise<{ signature: string; didKeyUri: DidResourceUri }>;
+}
+
 export {};
 declare global {
   interface Window {
-    kilt: any;
+    kilt: Record<string, InjectedWindowProvider>;
   }
 }
