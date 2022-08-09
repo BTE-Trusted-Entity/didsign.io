718874978aac79fcc1480894935b392d6d130fae
Luke Grunau <lukegrunau@gmail.com>
2022-08-09 16:01:42 +0200

    SK-1236: PR workflow (#52)

--- src/Utils/global.d.ts
+++ src/Utils/global.d.ts
@@ -1,6 +1,16 @@
+import { DidResourceUri, RequestForAttestation } from '@kiltprotocol/sdk-js';
+
+interface InjectedWindowProvider {
+  signWithDid: (plaintext: string) => Promise<{
+    signature: string;
+    didKeyUri: DidResourceUri;
+    credentials?: { name: string; credential: RequestForAttestation }[];
+  }>;
+}
+
 export {};
 declare global {
   interface Window {
-    kilt: any;
+    kilt: Record<string, InjectedWindowProvider>;
   }
 }
