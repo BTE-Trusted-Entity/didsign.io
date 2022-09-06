5b5f92ce335fc5afc2169e70d756d609ce07be54
Marton Csernai <marton@kilt.io>
2022-09-06 15:16:40 +0200

    finalize text

--- src/Components/Maintenance/Maintenance.tsx
+++ src/Components/Maintenance/Maintenance.tsx
@@ -4,19 +4,20 @@ export function Maintenance() {
   return (
     <div className={styles.container}>
       <div className={styles.contents}>
-        <h1 className={styles.heading}>We’ll be back soon</h1>
+        <h1 className={styles.heading}>
+          DIDsign is currently under maintenance, as KILT migrates from Kusama
+          to Polkadot.
+        </h1>
         <p className={styles.text}>
-          Sorry for the inconvenience but we’re performing some maintenance at
-          the moment.
+          <a
+            className={styles.anchor}
+            href="https://status.kilt.io/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Please check our status page for updates.
+          </a>
         </p>
-        <a
-          className={styles.anchor}
-          href="https://status.kilt.io/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Please check here for more info
-        </a>
       </div>
     </div>
   );
