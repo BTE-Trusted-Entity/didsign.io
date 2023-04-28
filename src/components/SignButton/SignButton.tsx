217218740d974b0341311ccbb02dde262739557c
Tom Adler <me@arty.name>
2023-04-28 11:52:11 +0200

    support other wallets (#265)

--- src/components/SignButton/SignButton.tsx
+++ src/components/SignButton/SignButton.tsx
@@ -7,7 +7,6 @@ import {
   createDidSignFile,
   createHashFromHashArray,
   generateJWS,
-  getSignatureContents,
 } from '../../utils/sign-helpers';
 import { useFiles } from '../Files/Files';
 import {
@@ -21,46 +20,46 @@ import {
 import { exceptionToError } from '../../utils/exceptionToError';
 import { useBooleanState } from '../../hooks/useBooleanState';
 
+import { SignWithDid } from '../../utils/types';
+
 export function SignButton() {
   const [signStatus, setSignStatus] = useState<
-    'SignError' | 'Default' | 'No Sporran' | 'NotAuthorized' | 'Rejected'
+    'SignError' | 'Default' | 'No Wallet' | 'NotAuthorized' | 'Rejected'
   >();
   const { files, setFiles } = useFiles();
   const { setSignature } = useSignature();
   const signPopup = useBooleanState();
   const isProcessing = useBooleanState();
 
-  const handleSign = useCallback(async () => {
-    if (files.length === 0) {
-      return;
-    }
-    isProcessing.on();
-    setSignStatus('Default');
+  const handleSign = useCallback(
+    async (signWithDid: SignWithDid) => {
+      if (files.length === 0) {
+        return;
+      }
+      isProcessing.on();
+      setSignStatus('Default');
 
-    try {
-      const hashes = files.map(({ hash }) => hash);
-      const signingData = await createHashFromHashArray(hashes);
+      try {
+        const hashes = files.map(({ hash }) => hash);
+        const signingData = await createHashFromHashArray(hashes);
 
-      const {
-        credentials = undefined,
-        didKeyUri,
-        signature,
-      } = await getSignatureContents(signingData);
+        const {
+          credentials = undefined,
+          didKeyUri,
+          signature,
+        } = await signWithDid(signingData);
 
-      const jws = generateJWS({ signature, didKeyUri }, signingData);
+        const jws = generateJWS({ signature, didKeyUri }, signingData);
 
-      const file = await createDidSignFile({ hashes, jws, credentials });
-      setFiles((files) => [file, ...files]);
+        const file = await createDidSignFile({ hashes, jws, credentials });
+        setFiles((files) => [file, ...files]);
 
-      setSignature((old) => ({
-        ...old,
-        signature,
-        ...(credentials && { credentials }),
-      }));
-    } catch (error) {
-      if (!window.kilt.sporran) {
-        setSignStatus('No Sporran');
-      } else {
+        setSignature((old) => ({
+          ...old,
+          signature,
+          ...(credentials && { credentials }),
+        }));
+      } catch (error) {
         const { message } = exceptionToError(error);
 
         if (message.includes('Not authorized')) {
@@ -74,26 +73,44 @@ export function SignButton() {
         }
 
         setSignStatus('SignError');
+      } finally {
+        isProcessing.off();
       }
-    } finally {
-      isProcessing.off();
-    }
-  }, [files, setFiles, setSignature, isProcessing]);
+    },
+    [files, setFiles, setSignature, isProcessing],
+  );
+
+  const handleNoWallet = useCallback(() => {
+    setSignStatus('No Wallet');
+  }, []);
+  const fakeWallet = { key: 'fake', name: 'Fake', handleClick: handleNoWallet };
 
   const handleDismiss = useCallback(() => {
     setSignStatus(undefined);
   }, []);
 
+  const capableWallets = [...Object.entries(window.kilt)]
+    .filter(([key]) => window.kilt[key].signWithDid)
+    .map(([key, { name = key, signWithDid }]) => ({
+      key,
+      name,
+      handleClick: () => handleSign(signWithDid),
+    }));
+  const buttons = capableWallets.length > 0 ? capableWallets : [fakeWallet];
+
   return (
     <div className={styles.container}>
       <div className={styles.buttonContainer}>
-        <button
-          className={styles.signButton}
-          disabled={files.length === 0 || isProcessing.current}
-          onClick={handleSign}
-        >
-          Sign
-        </button>
+        {buttons.map(({ key, name, handleClick }) => (
+          <button
+            key={key}
+            className={styles.signButton}
+            disabled={files.length === 0 || isProcessing.current}
+            onClick={handleClick}
+          >
+            {buttons.length === 1 ? 'Sign' : `Sign with ${name}`}
+          </button>
+        ))}
 
         <button
           className={styles.infoButton}
@@ -105,7 +122,7 @@ export function SignButton() {
 
         {signStatus === 'Default' && <SignPopup onDismiss={handleDismiss} />}
 
-        {signStatus === 'No Sporran' && (
+        {signStatus === 'No Wallet' && (
           <NoWalletPopup onDismiss={handleDismiss} />
         )}
 
