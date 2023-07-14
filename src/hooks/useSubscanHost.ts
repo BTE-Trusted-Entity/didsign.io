813cbbeaa637af66cf8e2fdd8b883f17a6bd1206
Tom Adler <me@arty.name>
2023-07-14 13:14:52 +0200

    no peregrine path

--- src/hooks/useSubscanHost.ts
+++ src/hooks/useSubscanHost.ts
@@ -1,6 +1,5 @@
 const subscanHosts: Record<string, string | undefined> = {
-  'wss://peregrine.kilt.io/parachain-public-ws':
-    'https://kilt-testnet.subscan.io',
+  'wss://peregrine.kilt.io': 'https://kilt-testnet.subscan.io',
   'wss://spiritnet.kilt.io': 'https://spiritnet.subscan.io',
   'wss://kilt-rpc.dwellir.com': 'https://spiritnet.subscan.io',
 };
