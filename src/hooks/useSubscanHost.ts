e94f723d46da0b6d00e75540f01888b88b699fd8
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2024-02-22 12:29:50 +0100

    fix: only show subscan link if target known (#553)
    * fix: only show subscan link if target known
    * chore: add onfinality to list of known full nodes

--- src/hooks/useSubscanHost.ts
+++ src/hooks/useSubscanHost.ts
@@ -2,11 +2,12 @@ const subscanHosts: Record<string, string | undefined> = {
   'wss://peregrine.kilt.io': 'https://kilt-testnet.subscan.io',
   'wss://spiritnet.kilt.io': 'https://spiritnet.subscan.io',
   'wss://kilt-rpc.dwellir.com': 'https://spiritnet.subscan.io',
+  'wss://spiritnet.api.onfinality.io': 'https://spiritnet.subscan.io',
 };
 
 export function useSubscanHost(): string | undefined {
   const kiltEndpoint =
     process.env.REACT_APP_CHAIN_ENDPOINT || 'wss://spiritnet.kilt.io';
 
-  return subscanHosts[kiltEndpoint];
+  return subscanHosts[new URL(kiltEndpoint).origin];
 }
